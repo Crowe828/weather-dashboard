99e4cbc49da9f11aa999049724e309735c895e84
frillic <69489498+crowe828@users.noreply.github.com>
2020-09-21 21:38:40 -0400

    built out main content for the five-day forecast

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,6 +7,10 @@
 // Prob build a URL with the API
 
 // Personal API key
+
+// Integrating moment.js to use their date features
+console.log(moment().format("l"));
+
 var APIKey = "e10987ffef94fe88a10c5d54823a1d23";
 
 // When search button is clicked, the weather is displayed for the searched for city
@@ -33,13 +37,11 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (weather) {
-      console.log(this.url);
       console.log(weather);
 
       // City name
-      console.log(weather.name);
       var cityName = document.querySelector(".city");
-      cityName.textContent = weather.name;
+      cityName.textContent = weather.name + moment().format("l");
 
       // Temperature
       var F = weather.main.temp.toFixed(1);
@@ -81,7 +83,6 @@ $(document).ready(function () {
         url: uvURL,
         method: "GET",
       }).then(function (uv) {
-        console.log(this.url);
         console.log(uv);
         var uv = uv.value.toFixed(1);
         console.log(uv);
@@ -104,38 +105,67 @@ $(document).ready(function () {
       url: forecastURL,
       method: "GET",
     }).then(function (forecast) {
-      console.log(this.url);
       console.log(forecast);
 
       // After user enters a city, the temperature and humidity will be shown for the next five days
       // Day 1
       var dayOne = forecast.list[4];
-      console.log(dayOne);
-      console.log(dayOne.main.temp);
+      var dayTitleOne = document.querySelector(".date-title-one");
+      var dayTempOne = document.querySelector(".date-temp-one");
+      var dayHumOne = document.querySelector(".date-hum-one");
+      dayTitleOne.textContent = moment().add(1, "day").format("l");
+      dayTempOne.textContent = dayOne.main.temp.toFixed(1);
+      dayHumOne.textContent = dayOne.main.humidity;
+      console.log(moment().add(1, "day").format("l"));
+      console.log(dayOne.main.temp.toFixed(1));
       console.log(dayOne.main.humidity);
 
       // Day 2
       var dayTwo = forecast.list[12];
-      console.log(dayTwo);
-      console.log(dayTwo.main.temp);
+      var dayTitleTwo = document.querySelector(".date-title-two");
+      var dayTempTwo = document.querySelector(".date-temp-two");
+      var dayHumTwo = document.querySelector(".date-hum-two");
+      dayTitleTwo.textContent = moment().add(2, "days").format("l");
+      dayTempTwo.textContent = dayTwo.main.temp.toFixed(1);
+      dayHumTwo.textContent = dayTwo.main.humidity;
+      console.log(moment().add(2, "days").format("l"));
+      console.log(dayTwo.main.temp.toFixed(1));
       console.log(dayTwo.main.humidity);
 
       // Day 3
       var dayThree = forecast.list[20];
-      console.log(dayThree);
-      console.log(dayThree.main.temp);
+      var dayTitleThree = document.querySelector(".date-title-three");
+      var dayTempThree = document.querySelector(".date-temp-three");
+      var dayHumThree = document.querySelector(".date-hum-three");
+      dayTitleThree.textContent = moment().add(3, "days").format("l");
+      dayTempThree.textContent = dayThree.main.temp.toFixed(1);
+      dayHumThree.textContent = dayThree.main.humidity;
+      console.log(moment().add(3, "days").format("l"));
+      console.log(dayThree.main.temp.toFixed(1));
       console.log(dayThree.main.humidity);
 
       // Day 4
       var dayFour = forecast.list[28];
-      console.log(dayFour);
-      console.log(dayFour.main.temp);
+      var dayTitleFour = document.querySelector(".date-title-four");
+      var dayTempFour = document.querySelector(".date-temp-four");
+      var dayHumFour = document.querySelector(".date-hum-four");
+      dayTitleFour.textContent = moment().add(4, "days").format("l");
+      dayTempFour.textContent = dayFour.main.temp.toFixed(1);
+      dayHumFour.textContent = dayFour.main.humidity;
+      console.log(moment().add(4, "days").format("l"));
+      console.log(dayFour.main.temp.toFixed(1));
       console.log(dayFour.main.humidity);
 
       // Day 5
       var dayFive = forecast.list[36];
-      console.log(dayFive);
-      console.log(dayFive.main.temp);
+      var dayTitleFive = document.querySelector(".date-title-five");
+      var dayTempFive = document.querySelector(".date-temp-five");
+      var dayHumFive = document.querySelector(".date-hum-five");
+      dayTitleFive.textContent = moment().add(5, "days").format("l");
+      dayTempFive.textContent = dayFive.main.temp.toFixed(1);
+      dayHumFive.textContent = dayFive.main.humidity;
+      console.log(moment().add(5, "days").format("l"));
+      console.log(dayFive.main.temp.toFixed(1));
       console.log(dayFive.main.humidity);
     });
   }
